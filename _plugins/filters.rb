97ecd96ba2272c3d7b1b89f2134067e007466b5a
Andrew Stone <andrew@clovar.com>
2013-11-01 22:38:45 -0400

    Removing stupid filter comment

--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -3,7 +3,6 @@ require 'liquid'
 module Jekyll
   module Filters
 
-    # Return the url's domain name
     def format_date(date, format=@context.registers[:site].config['date_format'])
       return date.strftime(format)
     end
